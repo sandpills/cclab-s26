fb1195d812a96ed7131c3d13d77d209221a4e627
viola he <viola@nyu.edu>
2026-04-14 15:30:00 +0800

    cloud with sound

---
+++ object-interact/firework.js
@@ -0,0 +1,39 @@
+
+
+class Firework {
+    constructor(startX, startY) {
+        this.x = startX;
+        this.y = startY;
+        this.size = random(2, 10);
+        this.hue = random(0, 360);
+
+        this.speedX = random(-3, 3);
+        this.speedY = random(-1, -3);
+
+        this.isDone = false;
+    }
+
+    checkBounds() {
+        if (this.x > width || this.x < 0 || this.y > height || this.y < 0) {
+            this.isDone = true;
+        }
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    display() {
+
+        push();
+        translate(this.x, this.y);
+
+        colorMode(HSB)
+        fill(this.hue, 80, 100)
+        noStroke();
+        circle(0, 0, this.size);
+
+        pop();
+        this.checkBounds();
+    }
+}
